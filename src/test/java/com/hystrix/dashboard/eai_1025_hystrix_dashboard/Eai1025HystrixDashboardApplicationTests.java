03fec2e95300b9f5c3c625185e831c887bf50fef
Mahesh-74 <mahesh74.java@gmail.com>
2024-06-07 14:53:52 +0530

    FM

---
+++ src/test/java/com/hystrix/dashboard/eai_1025_hystrix_dashboard/Eai1025HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hystrix.dashboard.eai_1025_hystrix_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eai1025HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
